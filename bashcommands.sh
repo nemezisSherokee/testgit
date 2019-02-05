8057bd5fd39501a590d4f659a59b832fcf9aa0b2
kamdoum <lary_xyahoo.fr>
2019-02-05 15:14:20 +0100

    commit_message

--- bashcommands.sh
+++ bashcommands.sh
@@ -8,3 +8,13 @@ function git_push_origin_master {
 }
 
 alias push="git_push_origin_master"
+
+function git_commit_messge {
+
+	test -n $1 && {
+	  echo "$1"
+	 git commit -m "$1"
+	}
+}
+
+alias commit="git_commit_messge"
